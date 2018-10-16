2da7befbc27d9f3a6b04668c074461801efbdd8d
sleungee <44163401+sleungee@users.noreply.github.com>
2018-10-16 15:54:28 +0100

    Update Model2.py

---
+++ Model2.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+x0=50
+y0=50
+import random
+#new line
+random_number = random.random()
+print (random_number)
+if random_number < 0.5:
+    y0 = y0 + 1
+else:
+    y0 = y0 - 1
+print (y0)
+
+
+import random
+random_number=random.random()
+print (random_number)
+if random_number < 0.5:
+    x0 = x0 + 1
+else:
+    x0 = x0 - 1
+print (x0)
+
+import random
+x0=50
+y0=50
+
+if random.random() < 0.5:
+    x0 +=1
+    x0 -=1
+    
+
+
+
+random_number = random.random()
+print (random_number)
+if random_number < 0.5:
+    y0 = y0 + 1
+else:
+    y0 = y0 - 1
+print (y0)
+
+
+import random
+random_number=random.random()
+print (random_number)
+if random_number < 0.5:
+    x0 = x0 + 1
+else:
+    x0 = x0 - 1
+print (x0)
+
+import random
+x0=50
+y0=50
+
+if random.random() < 0.5:
+    x0 +=1
+    x0 -=1
+    
+
